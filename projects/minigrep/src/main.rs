5d434d83f559a7c6bb32c57cdcc49db2dd552b77
Graham Doerksen <46031209+gdoerksen@users.noreply.github.com>
2023-03-25 10:48:06 -0600

    Placed functionality into lib
    Implemented fn search

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,39 +13,10 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Searching for {}", config.query);
-    println!("in file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-}
-
-}
